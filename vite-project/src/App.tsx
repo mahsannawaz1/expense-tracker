a771a6103c01b9893fd2d805f5209f86ebf68266
mahsannawaz1 <nawazehsen@gmail.com>
2024-02-08 23:05:27 +0500

    added state for products

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,10 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Form from "./components/Form";
+import { FieldValues } from "react-hook-form";
+import { useState } from "react";
+
+interface Product {
+  description: string;
+  amount: number;
+  category: string;
+}
 
 function App() {
   const categories: string[] = ["Groceries", "Utilities", "Entertainment"];
-  return <Form categories={categories} />;
+
+  const [products, setProduct] = useState<Product[]>([]);
+
+  const handleFormSubmit = (data: FieldValues) => {
+    const product: Product = {
+      description: data.description,
+      amount: data.amount,
+      category: data.category,
+    };
+    setProduct([...products, product]);
+  };
+
+  return <Form categories={categories} onFormSubmit={handleFormSubmit} />;
 }
 
 export default App;
--- vite-project/src/components/Form.tsx
+++ vite-project/src/components/Form.tsx
@@ -4,9 +4,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 interface Props {
   categories: string[];
+  onFormSubmit: (data: FieldValues) => void;
 }
 
-const Form = ({ categories }: Props) => {
+const Form = ({ categories, onFormSubmit }: Props) => {
   const schema = z.object({
     description: z
       .string()
@@ -15,21 +16,20 @@ const Form = ({ categories }: Props) => {
     amount: z.number({ invalid_type_error: "This field is required" }).min(0),
     category: z.string().min(1, "This field is required"),
   });
-
+  const clearForm = () => {
+    isSubmitSuccessful && reset();
+  };
   type FormData = z.infer<typeof schema>;
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitSuccessful },
+    reset,
   } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
-  console.log(errors);
-  const onFormSubmit = (data: FieldValues) => {
-    console.log(data);
-  };
-
+  clearForm();
   return (
     <form onSubmit={handleSubmit(onFormSubmit)}>
       <div className="mb-3">
@@ -42,6 +42,9 @@ const Form = ({ categories }: Props) => {
           type="text"
           className="form-control"
         />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
@@ -53,6 +56,9 @@ const Form = ({ categories }: Props) => {
           type="number"
           className="form-control"
         />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
@@ -66,6 +72,9 @@ const Form = ({ categories }: Props) => {
             </option>
           ))}
         </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
